6aae78405b3e1cb36f04481fefd0be47053a32dd
wiesmad <wmazurak@gmail.com>
2026-01-01 22:00:09 +0100

    _site to docs change

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
+// module.exports = function (eleventyConfig) {
+//   eleventyConfig.addPassthroughCopy("public");
+// };
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('public');
+  eleventyConfig.addPassthroughCopy("public");
+
+  return {
+    dir: {
+      output: "docs",
+    },
+  };
 };
 
 // module.exports = function (eleventyConfig) {
