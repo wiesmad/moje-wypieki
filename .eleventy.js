39a698b754ddd2bfa3c4cb7469b89f9d5991f149
wiesmad <wmazurak@gmail.com>
2026-01-01 22:53:04 +0100

    ch2

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,20 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("public");
+  eleventyConfig.addPassthroughCopy({ public: "." });
 
   return {
     dir: {
       output: "docs",
     },
+    pathPrefix: "/moje-wypieki/", // <-- only for project pages
   };
 };
+
+// module.exports = function (eleventyConfig) {
+//   eleventyConfig.addPassthroughCopy("public");
+
+//   return {
+//     dir: {
+//       output: "docs",
+//     },
+//   };
+// };
